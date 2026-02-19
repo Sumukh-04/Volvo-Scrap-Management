b8cdbd89b43157368fa8a23e60239e9c2c6874a4
christy.jubi <christy.jubi@yuccasolutions.com>
2026-02-19 11:07:38 +0530

    FilterModal added

--- src/Modules/Pwa1/InboundPages/Inbound.tsx
+++ src/Modules/Pwa1/InboundPages/Inbound.tsx
@@ -1,4 +1,4 @@
-// import syncIcon from "../../../assets/image-assets/"
+import syncIcon from "../../../assets/image-assets/sync.png"
 import AppLayout from "../../../layouts/AppLayout"
 import StatsRow from "../../../Common/DashboardComponents/StatsRow"
 import FilterBar from "../../../Common/DashboardComponents/Filterbar"
@@ -14,7 +14,7 @@ export default function Inbound() {
 
             <button className="btn btn-outline sync-btn">
               <img
-                // src={syncIcon}
+                src={syncIcon}
                 alt="Sync"
                 className="sync-icon"
               />
